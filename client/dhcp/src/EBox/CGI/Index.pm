ca33850d0ad410f0c3609976c85ebdc8e49f5473
Isaac Clerencia <iclerencia@ebox-platform.com>
2005-11-30 16:29:33 +0000

    DHCP 0.7
    git-svn-id: file:///home/koke/ebox2git/svn/tags/release-0.7@4809 866bc4c1-54da-0310-a958-9d5a9a0e8d04

---
+++ client/dhcp/src/EBox/CGI/Index.pm
@@ -0,0 +1,97 @@
+# Copyright (C) 2005  Warp Networks S.L., DBS Servicios Informaticos S.L.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License, version 2, as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+package EBox::CGI::DHCP::Index;
+
+use strict;
+use warnings;
+
+use base 'EBox::CGI::ClientBase';
+
+use EBox::Global;
+use EBox::Gettext;
+
+## arguments:
+## 	title [required]
+sub new {
+	my $class = shift;
+	my $self = $class->SUPER::new('title'    => 'DHCP',
+				      'template' => 'dhcp/index.mas',
+				      @_);
+	$self->{domain} = 'ebox-dhcp';
+	bless($self, $class);
+	return $self;
+}
+
+sub _process($) {
+	my $self = shift;
+	my $dhcp = EBox::Global->modInstance('dhcp');
+	my $net = EBox::Global->modInstance('network');
+	my @names = $self->cgi->param;
+
+	my $ifaces = $net->allIfaces();
+	my @iflist = ();
+
+	foreach (@{$ifaces}) {
+		if ($net->ifaceMethod($_) eq 'static') {
+			push(@iflist, { name => $_, alias => $net->ifaceAlias($_)});
+		}
+	}
+	my %iface;
+	my $gateway = "";
+	my $nameserver1 = "";
+	my $nameserver2 = "";
+	my $fixed = undef;
+	my $ranges = undef;
+
+	if(@iflist != 0) {
+		$iface{name} = $self->param("iface");
+		if(! defined($iface{name})) {
+			#if not specified in URL take first static iface
+			$iface{name} = $iflist[0]->{name};
+		}
+		foreach my $if (@iflist){
+			if($if->{name} eq $iface{name}){
+				my $name = $if->{name};
+				$iface{alias} = $net->ifaceAlias($name);
+				$iface{address} = $net->ifaceAddress($name);
+				$iface{netmask} = $net->ifaceNetmask($name);
+				$iface{network} = $net->ifaceNetwork($name);
+				print STDERR "bar1w\n";
+				$iface{init} = $dhcp->initRange($name);
+				print STDERR "bar1y\n";
+				$iface{end} = $dhcp->endRange($name);
+			}
+		}
+		$fixed = $dhcp->fixedAddresses($iface{name});
+		$ranges = $dhcp->ranges($iface{name});
+		$gateway = $dhcp->defaultGateway($iface{name});
+		$nameserver1 = $dhcp->nameserver($iface{name},1);
+		$nameserver2 = $dhcp->nameserver($iface{name},2);
+	}
+
+	my @array = ();
+	push (@array, 'iface'    	=> \%iface);
+	push (@array, 'ranges'    	=> $ranges);
+	push (@array, 'ifaces'    	=> \@iflist);
+	push (@array, 'fixed'    	=> $fixed);
+	push (@array, 'gateway'		=> $gateway);
+	push (@array, 'nameserver1'	=> $nameserver1);
+	push (@array, 'nameserver2'	=> $nameserver2);
+	push (@array, 'active'		=> $dhcp->service() ? 'yes' : 'no' );
+	$self->{params} = \@array;
+}
+
+1;
