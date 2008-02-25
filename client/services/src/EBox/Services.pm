d98298d88b6f83b9922cc6178a5f3cf811e5a7e4
Javier Uruen <juruen@ebox-platform.com>
2008-02-25 14:17:04 +0000

    merge from branches/ubuntu-0.11  -r8950:9687
    git-svn-id: file:///home/koke/ebox2git/svn/branches/ubuntu-0.11.99@9699 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- client/services/src/EBox/Services.pm
+++ client/services/src/EBox/Services.pm
@@ -222,6 +222,56 @@ sub addService
     return $self->{'serviceModel'}->addService(%params);
 }
 
+# Method: addMultipleService 
+#
+#   Add a multi protocol service to the services table	
+#
+# Parameters:
+#
+#   (NAMED)
+#   
+#   name        - service's name
+#   description - service's description
+#   internal - boolean, internal services can't be modified from the UI
+#   readOnly - boolean, set the row unremovable from the UI
+#
+#   services - array ref of hash ref containing:
+#
+#	    protocol    - it can take one of these: any, tcp, udp, 
+#	                                            tcp/udp, grep, icmp
+#	    sourcePort  - it can take:  "any"
+#                                   An integer from 1 to 65536 -> 22
+#                                   Two integers separated by colons -> 22:25 
+#	    destinationPort - same as source
+#
+#
+#	Example:
+#
+#       'name' => 'ssh',
+#       'description' => 'secure shell'.
+#       'services' => [ 
+#                       {
+#	                        'protocol' => 'tcp',
+#	                        'sourcePort' => 'any',
+#                           'destinationPort' => '21:22'
+#                        },
+#                        {
+#	                        'protocol' => 'tcp',
+#	                        'sourcePort' => 'any',
+#                           'destinationPort' => '21:22'
+#                        }
+#                     ];
+#
+#   Returns:
+#
+#   string - id of the new created row  
+sub addMultipleService 
+{
+    my ($self, %params) = @_;
+
+    return $self->{'serviceModel'}->addMultipleService(%params);
+}
+
 # Method: setService 
 #
 #   Set a existing service to the services table	
