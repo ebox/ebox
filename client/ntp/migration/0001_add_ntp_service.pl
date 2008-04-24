a3d951d07bbc8442fffeb6711ed1c54a8528cccf
Javier Amor <javier.amor.garcia@ebox-platform.com>
2008-04-24 14:04:21 +0000

    migration script refactored  refs #993
    git-svn-id: file:///home/koke/ebox2git/svn/branches/ubuntu-0.11.99@10123 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- client/ntp/migration/0001_add_ntp_service.pl
+++ client/ntp/migration/0001_add_ntp_service.pl
@@ -6,55 +6,22 @@ use base 'EBox::MigrationBase';
 use strict;
 use warnings;
 
-
-
 use EBox;
 use EBox::Global;
 use EBox::Gettext;
-use Data::Dumper;
-use EBox::Model::ModelManager;
-use Socket;
 use Error qw(:try);
-
-
-sub _addNTPService
-{
-
-    my $serviceMod = EBox::Global->modInstance('services');
-
-	if (not $serviceMod->serviceExists('name' => 'ntp')) {
-		 $serviceMod->addService('name' => 'ntp',
-			'protocol' => 'udp',
-			'sourcePort' => 'any',
-			'destinationPort' => 123,
-			'internal' => 1,
-			'readOnly' => 1);
-		
-	} else {
-          $serviceMod->setService('name' => 'ntp',
-			'protocol' => 'udp',
-			'sourcePort' => 'any',
-			'destinationPort' => 123,
-			'internal' => 1,
-			'readOnly' => 1);
-
-		EBox::info("Not adding ntp services as it already exists");
-	}
-
-    $serviceMod->saveConfig();
-}
-
 
 sub runGConf
 {
     my ($self) = @_;
 
-    $self->_addNTPService();
-
-    # add firewall rule for NTP service
-    my $fw = EBox::Global->modInstance('firewall');
-    $fw->setInternalService('ntp', 'accept');
-    $fw->save();
+    $self->addInternalService(
+			      'name' => 'ntp',
+			      'protocol' => 'udp',
+			      'sourcePort' => 'any',
+			      'destinationPort' => 123,
+			     );
+ 
 }
 
 EBox::init();
