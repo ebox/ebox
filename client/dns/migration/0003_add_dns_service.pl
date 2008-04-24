a9fb8aec9ec98fd8315d91d593ab0fa880c1c050
Javier Amor <javier.amor.garcia@ebox-platform.com>
2008-04-24 13:59:52 +0000

    refactored migration script  refs #993
    git-svn-id: file:///home/koke/ebox2git/svn/branches/ubuntu-0.11.99@10122 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- client/dns/migration/0003_add_dns_service.pl
+++ client/dns/migration/0003_add_dns_service.pl
@@ -7,60 +7,22 @@ use base 'EBox::MigrationBase';
 use strict;
 use warnings;
 
-
-
 use EBox;
 use EBox::Global;
 use EBox::Gettext;
-use Data::Dumper;
-use EBox::Model::ModelManager;
-use Socket;
 use Error qw(:try);
-
-
-
-
-sub _addDNSService
-{
-   my $serviceMod = EBox::Global->modInstance('services');
-
-   if (not $serviceMod->serviceExists('name' => 'dns')) {
-       $serviceMod->addService('name' => 'dns',
-           'protocol' => 'udp',
-           'sourcePort' => 'any',
-           'destinationPort' => 53,
-           'internal' => 1,
-	   'readOnly' => 1);
-
-   } else {
-       $serviceMod->setService('name' => 'dns',
-           'protocol' => 'udp',
-           'sourcePort' => 'any',
-           'destinationPort' => 53,
-           'internal' => 1,
-	   'readOnly' => 1);
-       EBox::info("Not adding dns service as it already exists");
-   }
-
-   $serviceMod->save();
-
-}
-
-
-
-sub _fwAllowDNSService
-{
-  my $fw = EBox::Global->modInstance('firewall');
-  $fw->setInternalService('dns', 'accept');
-  $fw->saveConfigRecursive();
-}
 
 sub runGConf
 {
   my ($self) = @_;
   
-  $self->_addDNSService();
-  $self->_fwAllowDNSService();
+  $self->addInternalService(
+			  'name' => 'dns',
+			  'protocol' => 'udp',
+			  'sourcePort' => 'any',
+			  'destinationPort' => 53,
+			 );
+
 }
 
 EBox::init();
