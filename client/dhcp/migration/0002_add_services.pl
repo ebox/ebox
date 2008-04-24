dc6d467e5cd63faefc627338e44223fbd06f97c8
Javier Amor <javier.amor.garcia@ebox-platform.com>
2008-04-24 14:07:14 +0000

    migration script refactored  refs #993
    git-svn-id: file:///home/koke/ebox2git/svn/branches/ubuntu-0.11.99@10124 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- client/dhcp/migration/0002_add_services.pl
+++ client/dhcp/migration/0002_add_services.pl
@@ -28,85 +28,25 @@ sub _createUserConfDir
   mkdir ($dir, 0755);
 }
 
-
-sub _addDHCPService
-{
-	my $serviceMod = EBox::Global->modInstance('services');
-
-	if (not $serviceMod->serviceExists('name' => 'dhcp')) {
-		 $serviceMod->addService('name' => 'dhcp',
-                        'description' => __('Dynamic Host Configuration Protocol'),
-			'protocol' => 'udp',
-			'sourcePort' => 'any',
-			'destinationPort' => 67,
-			'internal' => 1,
-			'readOnly' => 1);
-
-	} else {
-		 $serviceMod->setService('name' => 'dhcp',
-                        'description' => __('Dynamic Host Configuration Protocol'),
-			'protocol' => 'udp',
-			'sourcePort' => 'any',
-			'destinationPort' => 67,
-			'internal' => 1,
-			'readOnly' => 1);
-
-		EBox::info("Not adding dhcp services as it already exists");
-	}
-
-    $serviceMod->save();
-
-}
-
-# Add tftp service on ebox-services module
-sub _addTFTPService
-{
-    my $serviceMod = EBox::Global->modInstance('services');
-
-    if (not $serviceMod->serviceExists('name' => 'tftp')) {
-        $serviceMod->addService('name' => 'tftp',
-                                'description' => __('Trivial File Transfer Protocol'),
-                                'protocol' => 'udp',
-                                'sourcePort' => 'any',
-                                'destinationPort' => 69,
-                                'internal' => 1,
-                                'readOnly' => 1);
-
-    } else {
-        $serviceMod->setService('name' => 'tftp',
-                                'description' => __('Trivial File Transfer Protocol'),
-                                'protocol' => 'udp',
-                                'sourcePort' => 'any',
-                                'destinationPort' => 69,
-                                'internal' => 1,
-                                'readOnly' => 1);
-
-        EBox::info("Not adding tftp service as it already exists");
-    }
-
-    $serviceMod->save();
-
-}
-
-
-
-
-
-sub _fwAllowServices
-{
-  my $fw = EBox::Global->modInstance('firewall');
-  $fw->setInternalService('dhcp', 'accept');
-  $fw->setInternalService('tftp', 'accept');
-  $fw->saveConfigRecursive();
-}
-
 sub runGConf
 {
   my ($self) = @_;
 
-  $self->_addDHCPService();
-  $self->_addTFTPService();
-  $self->_fwAllowServices();
+  $self->addInternalService(
+			    'name' => 'tftp',
+			    'description' => __('Trivial File Transfer Protocol'),
+			    'protocol' => 'udp',
+			    'sourcePort' => 'any',
+			    'destinationPort' => 69,
+			   );
+
+  $self->addInternalService(
+			    'name' => 'dhcp',
+			    'description' => __('Dynamic Host Configuration Protocol'),
+			    'protocol' => 'udp',
+			    'sourcePort' => 'any',
+			    'destinationPort' => 67,
+			   );
   
   $self->_createUserConfDir();
 }
