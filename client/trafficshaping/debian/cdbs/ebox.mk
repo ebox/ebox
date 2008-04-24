f7cedd584c0d4a7873cf9690249e74d42086e3f4
Javier Uruen <juruen@ebox-platform.com>
2008-04-24 10:15:39 +0000

    remove postfix stuff from ebox.mk
    git-svn-id: file:///home/koke/ebox2git/svn/branches/ubuntu-0.11.99@10109 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- client/trafficshaping/debian/cdbs/ebox.mk
+++ client/trafficshaping/debian/cdbs/ebox.mk
@@ -1,6 +1,3 @@
-# change to yes when building the package with md quota support
-MD_QUOTA=no
-
 DEB_CONFIGURE_SCRIPT_ENV += LOGPATH="/var/log/ebox"
 DEB_CONFIGURE_SCRIPT_ENV += CONFPATH="/var/lib/ebox/conf"
 DEB_CONFIGURE_SCRIPT_ENV += STUBSPATH="/usr/share/ebox/stubs"
@@ -18,27 +15,3 @@ DEB_CONFIGURE_SCRIPT_ENV += MD_QUOTA="no"
 DEB_CONFIGURE_EXTRA_FLAGS := --disable-runtime-tests 
 DEB_MAKE_FLAGS += schemadir=usr/share/gconf/schemas
 DEB_MAKE_INVOKE = $(MAKE) $(DEB_MAKE_FLAGS) -C $(DEB_BUILDDIR)
-
-$(patsubst %,binary-install/%,$(DEB_PACKAGES)) :: binary-install/%:
-	for event in debian/*.upstart ; do \
-		[ -f $$event ] || continue; \
-		install -d -m 755 debian/$(cdbs_curpkg)/etc/event.d; \
-		DESTFILE=$$(basename $$(echo $$event | sed 's/\.upstart//g')); \
-		install -m 644 "$$event" debian/$(cdbs_curpkg)/etc/event.d/$$DESTFILE; \
-	done;
-
-
-postfix_deps=
-ifeq ($(MD_QUOTA), yes)
-	postfix_deps=postfix \(>= 2.4.5-3ubuntu1.ebox1\), postfix-ldap \(>= 2.4.5-3ubuntu1.ebox1\)
-  DEB_CONFIGURE_EXTRA_FLAGS += MD_QUOTA=yes
-else
-	postfix_deps=postfix, postfix-ldap
-endif
-
-
-
-
-binary-predeb/ebox-mail::
-	sed  "s/@POSTFIX_DEPS@/$(postfix_deps)/"   debian/control.in > debian/control 
-
