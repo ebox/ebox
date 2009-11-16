15509da3785e731af6cbc83ac44430c471f76a10
Javier Uruen <juruen@ebox-platform.com>
2009-11-16 12:26:45 +0000

    tagging remote services 1.2.2
    git-svn-id: file:///home/koke/ebox2git/svn/tags/1.2-series@15224 866bc4c1-54da-0310-a958-9d5a9a0e8d04

---
+++ remoteservices-1.2.2/debian/ebox-remoteservices.cron.d
@@ -0,0 +1,11 @@
+# /etc/cron.d/ebox-remoteservices: crontab entries for the ebox-remoteservices package
+
+SHELL=/bin/sh
+PATH=/usr/bin:/bin
+
+# Get the clients and their eBox assigned to our VPN server each minute
+0-59/15 * * * * root /usr/share/ebox/ebox-notify-mon-stats >> /dev/null 2>&1
+# Run the cron jobs sent by eBox CC
+0-59/10 * * * * root /usr/share/ebox/ebox-cronjob-runner >> /dev/null 2>&1
+# Get the new cron jobs from eBox CC
+10 3 * * * root /usr/share/ebox/ebox-get-cronjobs >> /dev/null 2>&1
