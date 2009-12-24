4b984553c0caee166c32535e1e7480db0e968f0c
Jose Antonio Calvo <jacalvo@ebox-platform.com>
2009-12-24 11:52:12 +0000

    tagging mail 1.3.13
    git-svn-id: file:///home/koke/ebox2git/svn/tags/1.3-series@15689 866bc4c1-54da-0310-a958-9d5a9a0e8d04

---
+++ mail-1.3.13/src/EBox/Mail/Model/SMTPOptions.pm
@@ -0,0 +1,318 @@
+# Copyright (C) 2007 Warp Networks S.L.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License, version 2, as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+
+package EBox::Mail::Model::SMTPOptions;
+use base 'EBox::Model::DataForm';
+
+use strict;
+use warnings;
+
+# eBox classes
+use EBox::Global;
+use EBox::Gettext;
+use EBox::Validate qw(:all);
+use EBox::Types::Int;
+use EBox::Types::Text;
+use EBox::Types::Password;
+use EBox::Types::Boolean;
+use EBox::Types::Host;
+use EBox::Types::Union;
+use EBox::Types::Union::Text;
+use EBox::Types::Port;
+use EBox::Types::Composite;
+use EBox::Types::MailAddress;
+
+# eBox exceptions used
+use EBox::Exceptions::External;
+
+use constant MAX_MSG_SIZE                          => '100';
+
+
+sub new
+{
+    my $class = shift @_ ;
+
+    my $self = $class->SUPER::new(@_);
+    bless($self, $class);
+
+    return $self;
+}
+
+
+# Method:  _table
+#
+# This method overrides <EBox::Model::DataTable::_table> to return
+# a table model description.
+#
+# This table is composed of two fields:
+#
+#   domain (<EBox::Types::Text>)
+#   enabled (EBox::Types::Boolean>)
+#
+# The only avaiable action is edit and only makes sense for 'enabled'.
+#
+sub _table
+{
+    my @tableDesc =
+        (
+         new EBox::Types::Text(
+                               fieldName => 'smarthost',
+                               printableName => __('Smarthost to send mail'),
+                               optional => 1,
+                               editable => 1,
+                               help     => __('The format is host[:port] being '
+                                              . 'port set to 25 if none is supplied'),
+                              ),
+         new EBox::Types::Union(
+                              fieldName => 'smarthostAuth',
+                              printableName =>
+                                __('Smarthost authentication'),
+                              editable => 1,
+# XXX Workaround to allow unsafe characters in Password.
+#     Union + Composite make difficult to call password method
+                              allowUnsafeChars => 1,
+                              subtypes => [
+                              new EBox::Types::Union::Text(
+                                  'fieldName' => 'none',
+                                  'printableName' => __('None'),
+                                  ),
+                              new EBox::Types::Composite(
+                                   fieldName => 'userandpassword',
+                                   printableName => __('User and password'),
+                                   editable => 1,
+                                   showTypeName => 0,
+                                   types => [
+                                             new EBox::Types::Text(
+                                              fieldName => 'username',
+                                              printableName => __('User'),
+                                              size => 20,
+                                              editable => 1,
+                                                                  ),
+                                             new EBox::Types::Password(
+                                              fieldName => 'password',
+                                              printableName => __('Password'),
+                                              size => 12,
+                                              editable => 1,
+                                              allowUnsafeChars => 1,
+                                                                  ),
+
+                                            ],
+                                                        )
+                                  ],
+             ),
+         new EBox::Types::MailAddress(
+             fieldName => 'bounceReturnAddress',
+             printableName => __('Return address for mail bounced back to the sender'),
+             defaultValue => 'noreply@example.com',
+             editable => 1,
+         ),
+         new EBox::Types::Union(
+                              fieldName => 'mailboxQuota',
+                              printableName =>
+                                __('Maximum mailbox size allowed'),
+                              help => 
+ __('When a mailbox reaches this size futher messages will be rejected. This can be overidden by account'),
+                              subtypes => [
+                              new EBox::Types::Union::Text(
+                                  'fieldName' => 'unlimited',
+                                  'printableName' => __('Unlimited size'),
+                                  ),
+                              new EBox::Types::Int(
+                                  'fieldName' => 'size',
+                                  'printableName' => __('size in Mb'),
+                                  'editable'  => 1,
+                                  'min'       => 1,
+                                      ),
+                                  ],
+             ),
+         new EBox::Types::Union(
+                              fieldName => 'maxSize',
+                              printableName =>
+                                __('Maximum message size accepted'),
+                              subtypes => [
+                              new EBox::Types::Union::Text(
+                                  'fieldName' => 'unlimitedMsgSize',
+                                  'printableName' => __('Unlimited size'),
+                                  ),
+                              new EBox::Types::Int(
+                                  'fieldName' => 'msgSize',
+                                  'printableName' => __('size in Mb'),
+                                  'editable'  => 1,
+                                  'min'       => 1,
+                                  'max'       => MAX_MSG_SIZE,
+                                      ),
+                                  ],
+             ),
+         new EBox::Types::Union(
+                              fieldName => 'deletedExpire',
+                              printableName =>
+                                __('Expiration period for deleted mails'),
+                              subtypes => [
+                              new EBox::Types::Union::Text(
+                                  'fieldName' => 'never',
+                                  'printableName' => __('Never'),
+                                  ),
+                              new EBox::Types::Int(
+                                  'fieldName' => 'days',
+                                  'printableName' => __('days'),
+                                  'editable'  => 1,
+                                  'min'       => 1,
+                                      ),
+                                  ],
+             ),
+         new EBox::Types::Union(
+                              fieldName => 'spamExpire',
+                              printableName =>
+                                __('Expiration period for spam mails'),
+                              subtypes => [
+                              new EBox::Types::Union::Text(
+                                  'fieldName' => 'never',
+                                  'printableName' => __('Never'),
+                                  ),
+                              new EBox::Types::Int(
+                                  'fieldName' => 'days',
+                                  'printableName' => __('days'),
+                                  'editable'  => 1,
+                                  'min'       => 1,
+                                      ),
+                                  ],
+             ),
+
+        );
+
+      my $dataForm = {
+                      tableName          => __PACKAGE__->nameFromClass(),
+                      printableTableName => __('Options'),
+                      modelDomain        => 'Mail',
+                      defaultActions     => [ 'editField', 'changeView' ],
+                      tableDescription   => \@tableDesc,
+
+                     };
+
+
+
+    return $dataForm;
+}
+
+
+#
+# Method: maxMsgSize
+#
+#  Returns:
+#   - the maximum message size allowed by the server in Mb or zero if we do
+#      not have any limit set
+#
+sub maxMsgSize
+{
+    my ($self) = @_;
+
+    my $maxSize = $self->row()->elementByName('maxSize');
+    if ($maxSize->selectedType eq 'unlimitedMsgSize') {
+        return 0;
+    }
+
+    my $size = $maxSize->subtype()->value();
+    $size .= 'M';
+    return $size;
+}
+
+
+# Method: maiboxQuota
+#
+#   get the default maximum size for an account's mailbox.
+#
+#   Returns:
+#      the amount in Mb or 0 for unlimited size
+sub mailboxQuota
+{
+    my ($self) = @_;
+
+    my $mailboxQuota = $self->row()->elementByName('mailboxQuota');
+    if ($mailboxQuota->selectedType eq 'unlimited') {
+        # 0 means unlimited for dovecot's quota plugin..
+        return 0;
+    }
+
+    my $size = $mailboxQuota->subtype()->value();
+    return $size;
+}
+
+
+sub expirationForDeleted
+{
+    my ($self) = @_;
+    return $self->_expiration('deletedExpire');
+}
+
+sub expirationForSpam
+{
+    my ($self) = @_;
+    return $self->_expiration('spamExpire');
+}
+
+
+sub _expiration
+{
+    my ($self, $element) = @_;
+    my $expiration = $self->row()->elementByName($element);
+    if ($expiration->selectedType eq 'never') {
+        # 0 means unlimited 
+        return 0;
+    }
+
+    my $days = $expiration->subtype()->value();
+    return $days;
+}
+
+sub validateTypedRow
+{
+    my ($self, $action, $changedFields) = @_;
+    if (exists $changedFields->{smarthost}) {
+        $self->_validateSmarthost($changedFields);
+    }
+
+}
+
+
+sub _validateSmarthost
+{
+    my ($self, $changedFields) = @_;
+
+    my $smarthost = $changedFields->{smarthost}->value();
+    if (not $smarthost) {
+        # no smarthost, correct..
+        return undef;
+    }
+
+
+    if ($smarthost =~ m/:/) {
+        my ($host, $port) = split ':', $smarthost;
+        EBox::Validate::checkHost($host, __(q{Smarthost's address}));
+        EBox::Validate::checkPort($port, __(q{Smarthost's port}));
+        
+    }else {
+        EBox::Validate::checkHost($smarthost, __(q{Smarthost's address}));
+        
+    }
+
+
+}
+
+
+
+1;
+
---
+++ mail-1.3.13/src/EBox/MailLogHelper.pm
@@ -0,0 +1,299 @@
+# Copyright (C) 2005 Warp Networks S.L., DBS Servicios Informaticos S.L.
+#
+# This program is free software; you can redistribute it and/or modify
+
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+package EBox::MailLogHelper;
+
+use strict;
+use warnings;
+
+use EBox::Gettext;
+
+use base qw(EBox::LogHelper);
+
+use constant MAILOG => "/var/log/mail.log";
+use constant TABLENAME => "mail_message";
+
+# Table structure:
+# CREATE TABLE message (
+#        timestamp TIMESTAMP NOT NULL,
+#        message_id VARCHAR(340),
+#        client_host_ip INET NOT NULL,
+#        client_host_name VARCHAR(255) NULL,
+#        from_address VARCHAR(320),
+#        to_address VARCHAR(320) NOT NULL,
+#        message_size BIGINT,
+#        relay VARCHAR(320),
+#        message_type VARCHAR(10) NOT NULL,
+#        status VARCHAR(25) NOT NULL,
+#        message TEXT NOT NULL
+#);
+ 
+my %temp;
+
+sub new
+{
+    my $class = shift;
+    my $self = {};
+    bless($self, $class);
+    return $self;
+}
+
+# Method: domain
+#       
+#       Must return the text domain which the package belongs to
+#
+sub domain 
+{
+    return 'ebox-mail'; 
+}
+
+sub logFiles {
+    return [MAILOG];
+}
+
+# Method: _getDate
+#
+#  This method returns the date and time on database format.
+#
+# Returns:
+#
+#               string - yyyy-mm-dd hh:mm:ss format.
+sub _getDate
+{
+    my ($self, $line) = @_;
+
+    my @date = localtime(time);
+    
+    my $year = $date[5] + 1900;
+    my ($month, $day, $hour, $min, $sec) = $line =~ m/^(...) +(\d+) (..):(..):(..).*$/;
+    
+    return "$year-$month-$day $hour:$min:$sec";
+}
+
+# I need go deeper in postfix logs to get this stuff work better
+sub processLine
+{
+    my ($self, $file, $line, $dbengine) = @_;
+
+
+    if (not $line =~ m/(?:postfix)|(?:deliver)/) {
+        return;
+    }
+
+    if ($line =~ m/NOQUEUE/) {
+        # no admited to the queue, inserte error event
+        my ($who, $hostname, $clientip, $msg, $line2) = $line =~ m/.*NOQUEUE: reject: (.*) from (.*)\[(.*)\]: (.*); (.*)$/;
+        my ($from, $to) = $line2 =~ m/.*from=<(.*)> to=<(.*)> .*/;
+        
+        my $event = 'other';
+        if ($msg =~ m/.*550.*$/) {
+            $event = 'noaccount';
+        } elsif ($msg =~ m/.*554.*$/) {
+            $event = 'norelay';
+        } elsif ($msg =~ m/.*552.*$/) {
+            $event = 'maxmsgsize';
+        } elsif ($msg =~ m/Greylisted/) {
+            $event = 'greylist';
+        }
+ 
+        my $values = {
+                      timestamp => $self->_getDate($line),
+                      client_host_ip => $clientip,
+                      client_host_name => $hostname,
+                      from_address => $from,
+                      to_address => $to,
+                      status => 'reject',
+                      message => $msg,
+                      event => $event,
+                     };
+
+        $self->_insert($dbengine, $values);
+
+                
+    } elsif ($line =~ m/SASL PLAIN authentication failed/) {
+        # auth failed, not admited at queue. Insert noauth event
+        my ($hostname, $clientip) = $line =~ m/.*postfix\/.*: warning: (.*)\[(.*)\]: .*$/;
+                        
+        my $values = {
+                      timestamp => $self->_getDate($line),
+                      client_host_ip => $clientip,
+                      client_host_name => $hostname,
+                      event => 'noauth',
+                     };
+
+        $self->_insert($dbengine, $values);
+
+    } elsif ($line =~ m/cleanup.*message-id=/) {
+        # cleanup: removed for the quue and mail gets a messge id
+        my ($qid, $msg_id) = $line =~ m/.*: (.*): message\-id=<(.*)>.*$/;
+        exists $temp{$qid} or
+            return;
+
+        $temp{$qid}{'msgid'} = $msg_id;
+    } elsif ($line =~ m/deliver\((.*?)\): msgid=<(.*?)>: rejected: Quota exceeded \((.*?)\)/) {
+        # quta exceeded, insert maxusrsize event
+        my $to    = $1;
+        my $msgid = $2;
+        my $msg = $3; # XXX thois not works!
+        my $qid = _qidFromMessageId($msgid);
+        defined $qid or
+            return;
+        exists $temp{$qid} or
+            return;
+
+        $temp{$qid}{'to'}    = $to;
+        $temp{$qid}{'event'} = 'maxusrsize';
+        $temp{$qid}{'status'} = 'rejected';
+        $temp{$qid}{'message'} = $msg;
+        $temp{$qid}{'date'} = $self->_getDate($line);
+        $temp{$qid}{'relay'} = 'dovecot';
+
+        $self->_insertEvent($qid, $dbengine);
+    } elsif ($line =~ m/client=/) {
+        # this is the ponit of entry for messages, we could only get a new qid
+        # here 
+
+        my ($qid, $hostname, $clientip) = ($line =~ m/.*postfix\/.*: (.*): client=(.*)\[(.*)\]/);
+
+        $temp{$qid}{'qid'}      = $qid;
+        $temp{$qid}{'hostname'} = $hostname;
+        $temp{$qid}{'clientip'} = $clientip;
+        $temp{$qid}{'date'} = $self->_getDate($line);
+
+    } elsif ($line =~ m/qmgr.*from=</) {
+        # get size
+        my ($qid, $from, $size) = $line =~ m/.*: (.*): from=<(.*)>, size=(.*),.*$/;
+        exists $temp{$qid} or
+            return;
+
+        $temp{$qid}{'from'} = $from;
+        $temp{$qid}{'size'} = $size;
+    } elsif ($line =~ m/.*: (.*): to=<(.*)>, relay=(.*?), .*, status=(.*?) \((.*)\)$/) {
+        # to, relay, date, msg and status
+        my ($qid, $to, $relay, $status, $msg) = ($1, $2, $3, $4, $5);
+        exists $temp{$qid} or
+            return;
+
+        $temp{$qid}{'to'} = $to;
+        $temp{$qid}{'relay'} = $relay;
+        $temp{$qid}{'status'} = $status;
+        $temp{$qid}{'msg'} = $msg;
+        $temp{$qid}{'date'} = $self->_getDate($line); # XXX remove
+
+        if ($status ne 'sent') {
+            if ($msg =~ m/Connection timed out/) {
+                $temp{$qid}{'event'} = 'nohost';
+            }
+            elsif ($msg =~ /host.*said.*Relay access denied/) {
+                $temp{$qid}{'event'} = 'nosmarthostrelay';        
+            }
+            elsif ($msg =~ /server.*said.*authentication failure/) {
+                $temp{$qid}{'event'} = 'nosmarthostrelay';        
+            }
+            elsif ($msg =~ /Greylisted/) {
+                $temp{$qid}{'event'} = 'greylist';        
+            }
+            else {
+                $temp{$qid}{'event'} = 'other';
+            }
+
+        }
+
+        if (exists $temp{$qid}{'event'}) {
+            $self->_insertEvent($qid,  $dbengine);
+        }
+
+    } elsif ($line =~ m/.*removed.*/) {
+        # removed, last time we see the message insert event if it was not done before
+        my ($qid) = $line =~ m/.*qmgr.*: (.*): removed/;
+        exists $temp{$qid} or
+            return;
+
+        $temp{$qid}{'event'} = 'msgsent';
+        $self->_insertEvent($qid, $dbengine);
+    }
+
+}
+
+sub _qidFromMessageId
+{
+    my ($msgId) = @_;
+    foreach my $mail (values %temp) {
+        exists $mail->{'msgid'} or 
+            next;
+        if ($mail->{'msgid'} eq $msgId) {
+            return $mail->{'qid'};
+        }
+    }
+
+    return undef;
+}
+
+sub _insertEvent
+{
+    my ($self, $qid, $dbengine) = @_;
+
+
+
+    my $values = {
+                  timestamp => $temp{$qid}{'date'},
+                  message_id => $temp{$qid}{'msgid'},
+                  client_host_ip => $temp{$qid}{'clientip'},
+                  client_host_name => $temp{$qid}{'hostname'},
+                  from_address => $temp{$qid}{'from'},
+                  to_address => $temp{$qid}{'to'},
+                  message_size => $temp{$qid}{'size'},
+                  relay => $temp{$qid}{'relay'},
+                  status => $temp{$qid}{'status'},
+                  message => $temp{$qid}{'msg'},
+                  event => $temp{$qid}{'event'},
+                 };
+
+    delete $temp{$qid};
+
+    $self->_insert($dbengine, $values);
+}
+
+sub _insert
+{
+    my ($self, $dbengine, $values) = @_;
+
+    my $mail = EBox::Global->modInstance('mail');
+    my %vdomains = map { $_ => 1 } $mail->{vdomains}->vdomains();
+
+    my $type;
+    if (defined($values->{'from_address'}) and
+            defined($values->{'to_address'})) {
+        my $from = $values->{'from_address'};
+        my $to = $values->{'to_address'};
+        my($from_user, $from_domain) = split('@', $from);
+        my($to_user, $to_domain) = split('@', $to);
+        if(exists $vdomains{$from_domain} and
+                exists $vdomains{$to_domain}) {
+            $type = 'internal';
+        } elsif (exists $vdomains{$from_domain}) {
+            $type = 'sent';
+        } elsif (exists $vdomains{$to_domain}) {
+            $type = 'received';
+        } else {
+            $type = 'relay';
+        }
+    } else {
+        $type = 'unknown';
+    }
+    $values->{'message_type'} = $type;
+    $dbengine->insert(TABLENAME, $values);
+}
+
+1;
