1c041431f6d5c990d9185a91ffc3f71d2e33a03a
Jose Antonio Calvo <jacalvo@ebox-platform.com>
2010-01-16 21:59:43 +0000

    tagging mail 1.3.14
    git-svn-id: file:///home/koke/ebox2git/svn/tags/1.3-series@16040 866bc4c1-54da-0310-a958-9d5a9a0e8d04

---
+++ mail-1.3.14/src/EBox/Mail/Model/RetrievalServices.pm
@@ -0,0 +1,175 @@
+# Copyright (C) 2007 Warp Networks S.L.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License, version 2, as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+package EBox::Mail::Model::RetrievalServices;
+use base 'EBox::Model::DataForm';
+
+use strict;
+use warnings;
+
+# eBox classes
+use EBox::Global;
+use EBox::Gettext;
+use EBox::Validate qw(:all);
+use EBox::Types::Boolean;
+use EBox::Types::Select;
+
+
+
+# eBox exceptions used
+use EBox::Exceptions::External;
+
+
+# XXX TODO: disable ssl options when no service is enabled
+sub new
+{
+    my $class = shift @_ ;
+
+    my $self = $class->SUPER::new(@_);
+    bless($self, $class);
+
+    return $self;
+}
+
+
+# Method:  _table
+#
+# This method overrides <EBox::Model::DataTable::_table> to return
+# a table model description.
+#
+# This table is composed of two fields:
+#
+#   domain (<EBox::Types::Text>)
+#   enabled (EBox::Types::Boolean>)
+#
+# The only avaiable action is edit and only makes sense for 'enabled'.
+#
+sub _table
+{
+    my @tableDesc =
+        (
+         new EBox::Types::Boolean(
+                                  fieldName => 'pop3',
+                                  printableName => __('POP3 service enabled'),
+                                  editable => 1,
+                                  defaultValue => 1,
+                                 ),
+         new EBox::Types::Boolean(
+                                  fieldName => 'pop3s',
+                                  printableName => __('Secure POP3S service enabled'),
+                                  editable => 1,
+                                  defaultValue => 1,
+                                 ),
+         new EBox::Types::Boolean(
+                                  fieldName => 'imap',
+                                  printableName => __('IMAP service enabled'),
+                                  editable => 1,
+                                  defaultValue => 1,
+                                 ),
+         new EBox::Types::Boolean(
+                                  fieldName => 'imaps',
+                                  printableName => __('Secure IMAPS service enabled'),
+                                  editable => 1,
+                                  defaultValue => 1,
+                                 ),
+         new EBox::Types::Boolean(
+                                  fieldName => 'fetchmail',
+                                  printableName => __('Retrieve mail for external accounts'),
+                                  help =>
+ __(q{This allow users to retrieve mail for external accounts, the mail would be delivered to their local account. External account can be configured in the user's corner.} ),
+                                  editable => 1,
+                                 ),
+         new EBox::Types::Boolean(
+                                  fieldName => 'managesieve',
+                                  printableName => __('Manage Sieve scripts'),
+                                  help =>
+ __(q{This service allows to a use to manage his Sieve mail filtering scripts from a local client which speaks the ManageSieve protocol} ),
+                                  editable => 1,
+                                  defaultValue => 1,
+                                 ),
+        );
+
+      my $dataForm = {
+                      tableName          => __PACKAGE__->nameFromClass(),
+                      printableTableName => __('Mail retrieval services'),
+                      modelDomain        => 'Mail',
+                      defaultActions     => [ 'editField', 'changeView' ],
+                      tableDescription   => \@tableDesc,
+
+                     };
+
+
+
+    return $dataForm;
+}
+
+
+sub activeProtocos
+{
+    my ($self) = @_;
+    my @protocols;
+
+    if ($self->pop3Value()) {
+        push @protocols, 'pop3';
+    }
+
+    if ($self->pop3sValue()) {
+        push @protocols, 'pop3s';
+    }
+
+    if ($self->imapValue()) {
+        push @protocols, 'imap';
+    }
+
+    if ($self->imapsValue()) {
+        push @protocols, 'imaps';
+    }
+
+    if ($self->managesieveValue()) {
+        push @protocols, 'managesieve';
+    }
+
+    return \@protocols;
+}
+
+
+
+sub validateTypedRow
+{
+    my ($self, $action, $params_r, $actual_r) = @_;
+
+    # validate IMAP services changes
+    if ((not exists $params_r->{imap}) and (not exists $params_r->{imaps}) ) {
+        return;
+    }
+    
+    my $imap = exists $params_r->{imap} ? $params_r->{imap}->value() :
+                                          $actual_r->{imap}->value(); 
+    my $imaps = exists $params_r->{imaps} ? $params_r->{imaps}->value() :
+                                          $actual_r->{imaps}->value();   
+
+    my $global = EBox::Global->getInstance();
+
+    foreach my $mod (@{ $global->modInstances()  }) {
+        if ($mod->can('validateIMAPChanges') and $mod->isEnabled()) {
+            $mod->validateIMAPChanges($imap, $imaps);
+        }
+    }
+}
+
+
+1;
+
---
+++ mail-1.3.14/src/EBox/Mail/Model/VDomains.pm
@@ -0,0 +1,247 @@
+# Copyright (C) 2005 Warp Networks S.L., DBS Servicios Informaticos S.L.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License, version 2, as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+package EBox::Mail::Model::VDomains;
+
+use strict;
+use warnings;
+
+use base 'EBox::Model::DataTable';
+
+# Class: EBox::Mail::Model::VDomains
+#
+#       This a class used it as a proxy for the vodmains stored in LDAP.
+#       It is meant to improve the user experience when managing vdomains,
+#       but it's just a interim solution. An integral approach needs to
+#       be done.
+#
+use EBox::Global;
+use EBox::Gettext;
+use EBox::Validate qw(:all);
+use EBox::Exceptions::External;
+
+use EBox::Mail::Types::WriteOnceDomain;
+use EBox::Types::HasMany;
+
+
+sub new
+{
+        my $class = shift;
+        my %parms = @_;
+
+        my $self = $class->SUPER::new(@_);
+        bless($self, $class);
+
+        return $self;
+}
+
+sub _table
+{
+        my @tableHead =
+         (
+
+                new EBox::Mail::Types::WriteOnceDomain(
+                                        'fieldName' => 'vdomain',
+                                        'printableName' => __('Name'),
+                                        'size' => '20',
+                                        'editable' => 1,
+                                        'unique' => 1,
+                                      ),
+               new EBox::Types::HasMany(
+                      fieldName => 'aliases',
+                       printableName => __('Virtual domain aliases'),
+                      foreignModel => 'mail/VDomainAliases',
+                      'view' => '/ebox/Mail/View/VDomainAliases',
+                      'backView' => '/ebox/Mail/View/VDomains',
+                     ),
+               new EBox::Types::HasMany(
+                      fieldName => 'externalAliases',
+                       printableName => __('External accounts aliases'),
+                      foreignModel => 'mail/ExternalAliases',
+                      'view' => '/ebox/Mail/View/ExternalAliases',
+                      'backView' => '/ebox/Mail/View/VDomains',
+                     ),
+               new EBox::Types::HasMany(
+                      fieldName => 'settings',
+                       printableName => __('Settings'),
+                      foreignModel => 'mail/VDomainSettings',
+                      'view' => '/ebox/Mail/View/VDomainSettings',
+                      'backView' => '/ebox/Mail/View/VDomains',
+                     ),
+
+
+         );
+
+        my $dataTable =
+                {
+                        'tableName' => 'VDomains',
+                        'printableTableName' => __('List of Domains'),
+                        'pageTitle'         => __('Virtual Domains'),
+                        'HTTPUrlView'       => 'Mail/View/VDomains',
+                        'defaultController' =>
+            '/ebox/Mail/Controller/VDomains',
+                        'defaultActions' =>
+                                ['add', 'del', 'changeView'],
+                        'tableDescription' => \@tableHead,
+                        'menuNamespace' => 'Mail/VDomains',
+                        'automaticRemove'  => 1,
+                        'help' => '',
+                        'printableRowName' => __('virtual domain'),
+                        'sortedBy' => 'vdomain',
+                        'messages' => { add => __('Virtual domain added. ' .
+                                'You must save changes to use this domain')
+                            },
+                };
+
+        return $dataTable;
+}
+
+# Method: precondition
+#
+#       Check if the module is configured
+#
+# Overrides:
+#
+#       <EBox::Model::DataTable::precondition>
+sub precondition
+{
+        my $mail = EBox::Global->modInstance('mail');
+        return $mail->configured();
+}
+
+# Method: preconditionFailMsg
+#
+#       Check if the module is configured
+#
+# Overrides:
+#
+#       <EBox::Model::DataTable::precondition>
+sub preconditionFailMsg
+{
+        return __('You must enable the mail module in module ' .
+                  'status section in order to use it.');
+}
+
+
+sub alwaysBccByVDomain
+{
+    my ($self) = @_;
+    my %alwaysBcc;
+
+    my @ids = @{ $self->ids() };
+    foreach my $id (@ids) {
+        my $row = $self->row($id);
+        my $settings = $row->elementByName('settings')->foreignModelInstance();
+        my $address  = $settings->bccAddress();
+        if ($address) {
+            my $vdomain = $row->valueByName('vdomain');
+            $alwaysBcc{$vdomain} = $address;
+            # add vdomain alias too
+            my $vdomainAlias = $row->elementByName('aliases')->foreignModelInstance();
+            my @aliases = @{ $vdomainAlias->aliases() };
+            foreach my $alias (@aliases) {
+                $alwaysBcc{$alias} = $address;
+            }
+        }
+    }
+
+    return \%alwaysBcc;
+}
+
+sub alwaysBcc
+{
+    my ($self) = @_;
+    my %alwaysBcc;
+
+    my @ids = @{ $self->ids() };
+    foreach my $id (@ids) {
+        my $row = $self->row($id);
+        my $settings = $row->elementByName('settings')->foreignModelInstance();
+        my $address  = $settings->bccAddress();
+        if ($address) {
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
+
+sub validateTypedRow
+{
+    my ($self, $action, $changedFields, $allFields) = @_;
+
+    if (not exists $changedFields->{vdomain}) {
+        return;
+    }
+
+    my $vdomain = $changedFields->{vdomain}->value();
+    if ($self->existsVDomainAlias($vdomain)) {
+        throw EBox::Exceptions::External(
+__x('Cannot add virtual domain {vd} because is a virtual domain alias with the same name',
+   vd => $vdomain)
+                                        );
+    }
+
+    $self->_checkVDomainIsNotInExternalAliases($vdomain);
+}
+
+
+
+
+sub existsVDomain
+{
+    my ($self, $vdomain) = @_;
+
+    my $res = $self->findValue(vdomain => $vdomain);
+    return defined $res;
+}
+
+
+sub existsVDomainAlias
+{
+    my ($self, $alias) = @_;
+    foreach my $id (@{ $self->ids() }) {
+        my $row = $self->row($id);
+        my $vdomainAlias = $row->elementByName('aliases')->foreignModelInstance();
+        if ($vdomainAlias->existsAlias($alias)) {
+            return 1;
+        }
+    }
+
+    return undef;
+}
+
+sub _checkVDomainIsNotInExternalAliases
+{
+    my ($self, $vdomain) = @_;
+    foreach my $id (@{ $self->ids() }) {
+        my $row = $self->row($id);
+        my $externalAliases = $row->elementByName('externalAliases')->foreignModelInstance();
+        my $alias = $externalAliases->firstAliasForExternalVDomain($vdomain);
+        if ($alias) {
+            throw EBox::Exceptions::External(
+                                             __x(
+'Cannot add virtual domain {vd} because it appears as external domain in the account referecned by the alias {al}',
+vd => $vdomain, al => $alias
+                                                )
+                                            );
+
+        }
+    }
+}
+
+1;
+
