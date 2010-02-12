78b179f405316159bc9585e254b247874b6a557b
Jose Antonio Calvo <jacalvo@ebox-platform.com>
2010-02-12 04:17:24 +0000

    tagging mail 1.4.2
    git-svn-id: file:///home/koke/ebox2git/svn/tags/1.4-series@16692 866bc4c1-54da-0310-a958-9d5a9a0e8d04

---
+++ mail-1.4.2/migration/0006_update_db.pl
@@ -0,0 +1,37 @@
+#!/usr/bin/perl
+
+#	Migration between gconf data version 5 to 6
+#
+#
+#   This migration script changes the names of some fields and tables
+#
+package EBox::Migration;
+use base 'EBox::Migration::Base';
+
+use strict;
+use warnings;
+
+use EBox;
+use EBox::Config;
+use EBox::Global;
+use EBox::Migration::Helpers;
+
+sub runGConf
+{
+    EBox::Migration::Helpers::dropIndex('timestamp_i');
+    EBox::Migration::Helpers::renameField('message', 'postfix_date', 'timestamp');
+    EBox::Migration::Helpers::renameTable('message', 'mail_message');
+    EBox::Migration::Helpers::renameConsolidationTable('mail_traffic', 'mail_message_traffic');
+    EBox::Migration::Helpers::createTimestampIndex('mail_message');
+    EBox::Migration::Helpers::addColumn('mail_message', 'message_type', 'VARCHAR(10) NOT NULL');
+}
+
+EBox::init();
+
+my $mod = EBox::Global->modInstance('mail');
+my $migration = new EBox::Migration(
+				    'gconfmodule' => $mod,
+				    'version' => 6,
+				   );
+
+$migration->execute();
