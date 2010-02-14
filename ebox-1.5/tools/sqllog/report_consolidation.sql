dd18a2e8f44e170617b28b23f5a9014afe81f654
Javier Uruen <juruen@ebox-platform.com>
2010-02-14 17:29:08 +0000

    remove ebox-1.5 tag
    git-svn-id: file:///home/koke/ebox2git/svn/tags/1.5-series@16787 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- ebox-1.5/tools/sqllog/report_consolidation.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE report_consolidation(
-        report_table VARCHAR(40) NOT NULL,
-        last_date TIMESTAMP NOT NULL
-
-);
--- ebox-1.5/tools/sqllog/sysinfo_disk_usage.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE sysinfo_disk_usage (
-    timestamp TIMESTAMP,
-    mountpoint VARCHAR(80),
-    used BIGINT,
-    free BIGINT
-);
-
-CREATE INDEX sysinfo_disk_usage_timestamp_i on sysinfo_disk_usage(timestamp);
