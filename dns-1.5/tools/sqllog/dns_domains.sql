6d847a3a9c4da15cdc86997b01fc9ae94cd4ca1c
Javier Uruen <juruen@ebox-platform.com>
2010-02-15 16:21:25 +0000

    remove tag dns-1.5
    git-svn-id: file:///home/koke/ebox2git/svn/tags/1.5-series@16821 866bc4c1-54da-0310-a958-9d5a9a0e8d04

--- dns-1.5/tools/sqllog/dns_domains.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE dns_domains (
-    timestamp TIMESTAMP,
-    domains INT
-);
-
-CREATE INDEX dns_domains_timestamp_i on dns_domains(timestamp);
--- dns-1.5/tools/sqllog/dns_domains_report.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE dns_domains_report (
-    date DATE,
-    domains INT
-);
