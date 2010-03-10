248b5238b1eb686e0e9058363e77226c7d9ce5e1
Jose Antonio Calvo <jacalvo@ebox-platform.com>
2010-03-10 16:47:55 +0000

    tagging ebox 1.4.4
    git-svn-id: file:///home/koke/ebox2git/svn/tags/1.4-series@17033 866bc4c1-54da-0310-a958-9d5a9a0e8d04

---
+++ ebox-1.4.4/src/EBox/Global/TestStub.pm
@@ -0,0 +1,93 @@
+package EBox::Global::TestStub;
+# Description:
+#
+use strict;
+use warnings;
+
+use Test::MockObject;
+use Params::Validate;
+use EBox::Global;
+use EBox::GConfModule::TestStub;
+
+
+my %modulesInfo;
+
+sub setAllEBoxModules
+{
+  my (%modulesByName) = @_;
+
+  while (my ($name, $module)  = each %modulesByName) {
+      setEBoxModule($name, $module);
+  }
+
+}
+
+sub setEBoxModule
+{
+    my ($name, $class, $depends) = @_;
+    validate_pos(@_ ,1, 1, 0);
+
+    defined $depends or
+        $depends = [];
+
+
+    $modulesInfo{$name} = {
+        class => $class,
+        depends => $depends,
+        changed => 0,
+       };
+
+
+
+}
+
+sub clear
+{
+    %modulesInfo = ();
+}
+
+sub _fakedReadModInfo
+{
+    my ($name) = @_;
+
+    if (exists $modulesInfo{$name}) {
+        return $modulesInfo{$name};
+    }
+
+    return undef;
+}
+
+
+sub  _fakedWriteModInfo
+{
+    my ($self, $name, $info) = @_;
+
+    $modulesInfo{$name} = $info;
+}
+
+
+sub _fakedModNames
+{
+    return [keys %modulesInfo];
+}
+
+sub fake
+{
+    EBox::GConfModule::TestStub::fake(); # needed by some method, like changed
+                                         # state of modules
+    Test::MockObject->fake_module('EBox::Global',
+                                  readModInfo => \&_fakedReadModInfo,
+                                  writeModInfo => \&_fakedWriteModInfo,
+                                  modNames     => \&_fakedModNames,
+                              );
+
+
+}
+
+# only for interface completion
+sub unfake
+{
+}
+
+
+1;
