d4415fadf2b08e4aa720c68c21cb0ff3e9cbd6b6
Jose Antonio Calvo <jacalvo@ebox-platform.com>
2010-03-12 12:11:28 +0000

    tagging installer 1.4.1
    git-svn-id: file:///home/koke/ebox2git/svn/tags/1.4-series@17051 866bc4c1-54da-0310-a958-9d5a9a0e8d04

---
+++ installer-1.4.1/generate_extras.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+test -r build_cd.conf || exit 1
+. ./build_cd.conf
+
+sudo rm -fr $CHROOT
+mkdir $CHROOT
+
+sudo debootstrap --arch=$ARCH $VERSION $CHROOT
+
+echo "deb http://archive.ubuntu.com/ubuntu hardy main restricted universe multiverse" > sources.list
+echo "deb http://archive.ubuntu.com/ubuntu hardy-updates main restricted universe multiverse" >> sources.list
+echo "deb http://security.ubuntu.com/ubuntu hardy-security main restricted universe" >> sources.list
+echo "deb http://ppa.launchpad.net/ebox/1.4/ubuntu hardy main" >> sources.list
+echo "deb http://ppa.launchpad.net/ebox/1.4-proposed/ubuntu hardy main" >> sources.list
+sudo mv sources.list $CHROOT/etc/apt/sources.list
+
+sudo chroot $CHROOT apt-get update
+
+test -r data/extra-packages.list || exit 1
+cat data/extra-packages.list | xargs sudo chroot $CHROOT apt-get install --download-only --allow-unauthenticated --yes
+
+cp $CHROOT/var/cache/apt/archives/*.deb $EXTRAS_DIR/
---
+++ installer-1.4.1/list-not-installed.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+
+# If we do a normal installation, without using LVM, cryptsetup or RAID, we should
+# avoid deleting the following packages
+MANDATORY_PACKAGES="
+lvm
+dmsetup
+devmapper
+crypt
+generic
+mdadm
+kpartx
+multipath
+lilo
+xfs
+jfs"
+
+cd /cdrom/pool/main
+
+for j in *
+do
+    cd $j
+    for k in *
+    do
+        cd $k
+        for l in *.deb
+        do
+            if [ $l != "*.deb" ]
+            then
+                n=$(dpkg -l $(echo $l | cut -f1 -d"_") 2> /dev/null| grep "^ii")
+                if [ -z "$n" ]
+                then
+                    pkgfile=/cdrom/pool/main/$j/$k/$l
+                    skip=0
+                    for i in $MANDATORY_PACKAGES
+                    do
+                        if echo $pkgfile | grep -q $i
+                        then
+                            skip=1
+                            break
+                        fi
+                    done
+                    if [ $skip -eq 0 ]
+                    then
+                        #size=`du $pkgfile`
+                        #echo "$size $pkgfile"
+                        echo $pkgfile
+                    fi
+                fi
+            fi
+        done
+        cd ..
+    done
+    cd ..
+done
+find -depth -type d -empty -exec echo {} \;
