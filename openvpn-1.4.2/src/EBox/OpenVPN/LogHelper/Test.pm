d9554c919938c20586cc46478f532714a5a50c79
Jose Antonio Calvo <jacalvo@ebox-platform.com>
2010-05-10 01:18:21 +0000

    tagging openvpn 1.4.2
    git-svn-id: file:///home/koke/ebox2git/svn/tags/1.4-series@17324 866bc4c1-54da-0310-a958-9d5a9a0e8d04

---
+++ openvpn-1.4.2/src/EBox/OpenVPN/LogHelper/Test.pm
@@ -0,0 +1,306 @@
+package EBox::OpenVPN::LogHelper::Test;
+use base 'EBox::Test::Class';
+
+use strict;
+use warnings;
+
+use EBox::OpenVPN;
+use EBox::OpenVPN::LogHelper;
+
+use Test::More;
+use Test::Exception;
+
+use lib '../../..';
+
+
+sub fakeConfig : Test(setup)
+{
+  my ($self) = @_;
+
+  my $confDir = $self->_confDir();
+  my @config = (
+		  '/ebox/modules/openvpn/userActive'  => 1,
+		  '/ebox/modules/openvpn/openvpn_bin'  => '/usr/sbin/openvpn',
+		  '/ebox/modules/openvpn/user'  => 'nobody',
+		  '/ebox/modules/openvpn/group' => 'nobody',
+		  '/ebox/modules/openvpn/conf_dir' => $confDir,
+
+		  '/ebox/modules/openvpn/server/macaco/active'    => 1,
+		  '/ebox/modules/openvpn/server/macaco/port'    => 1194,
+		  '/ebox/modules/openvpn/server/macaco/proto'   => 'tcp',
+
+		  '/ebox/modules/openvpn/server/baboon/active'    => 1,
+		  '/ebox/modules/openvpn/server/baboon/port'    => 1200,
+		  '/ebox/modules/openvpn/server/baboon/proto'   => 'tcp',
+		  '/ebox/modules/openvpn/server/baboon/local'   => 'ppp0',
+
+		  '/ebox/modules/openvpn/client/gibon/active'    => 1,
+		  '/ebox/modules/openvpn/client/gibon/port'   => 1294,
+		  '/ebox/modules/openvpn/client/gibon/proto'  => 'udp',
+
+		  '/ebox/modules/openvpn/client/titi/active'    => 0,
+		  '/ebox/modules/openvpn/client/titi/port'   => 1394,
+		  '/ebox/modules/openvpn/client/titi/proto'  => 'udp',
+		      );
+  EBox::Module::Service::TestStub::setConfig(@config);
+}
+
+# XXX this  must be deleted if #848 is fixed
+sub fakePopulateConfFiles : Test(startup)
+{
+  Test::MockObject->fake_module('EBox::OpenVPN::LogHelper',
+				_populateLogFiles => sub {
+				  my ($self) = @_;
+				   $self->{logFiles} = $self->_logFilesFromDaemons;
+				},
+			       );
+
+}
+
+
+sub tearDownConfig: Test(teardown)
+{
+  EBox::Module::Service::TestStub::setConfig();
+}
+
+sub _confDir
+{
+  return 'testdatas';
+}
+
+
+
+sub processLineTest : Test(24)
+{
+
+  my $openvpn   = EBox::OpenVPN->_create();
+  my $logHelper = new EBox::OpenVPN::LogHelper($openvpn);
+  my $dbEngine  = new FakeDBEngine;
+
+  my $macacoServer = $openvpn->server('macaco');
+  my $baboonServer = $openvpn->server('baboon');
+  my $gibonClient  = $openvpn->client('gibon');
+
+  # 2 tests for each case
+  my @cases = (
+	       {
+		line => 'Tue Aug 21 09:32:09 2007 Diffie-Hellman initialized with 1024 bit key',
+		file => $macacoServer->logFile(),
+		expected => undef,
+
+	       },
+
+	       # server initialized
+	       {
+		line => 'Tue Aug 21 09:23:15 2007 Initialization Sequence Completed',
+		file => $macacoServer->logFile(),
+		expected => {
+			     timestamp => 'Tue Aug 21 09:23:15 2007',
+			     daemon_name => 'macaco',
+			     daemon_type => 'server',
+			     event      => 'initialized',
+			    },
+	       },
+
+	       # client initialized
+	       {
+		line => 'Mon Aug 27 06:51:48 2007 Initialization Sequence Completed',
+		file => $gibonClient->logFile(),
+		expected => {
+			     timestamp => 'Mon Aug 27 06:51:48 2007',
+			     daemon_name => 'gibon',
+			     daemon_type => 'client',
+			     event      => 'initialized',
+			    },
+	       },
+
+	       # verificationOk events
+	       {
+		line => 'Tue Aug 21 08:51:45 2007 192.168.45.184:54817 VERIFY OK: depth=1, /C=ES/ST=Nation/L=Nowhere/O=monos/CN=Certification_Authority_Certificate',
+		file => $baboonServer->logFile(),
+		expected => undef,
+	       },
+	       {
+		line => 'Tue Aug 21 08:51:45 2007 192.168.45.184:54817 VERIFY OK: depth=0, /C=ES/ST=Nation/L=Nowhere/O=monos/CN=mandril',
+		file => $baboonServer->logFile(),
+		expected => undef,
+	       },
+
+
+	       # verification error: unknown ca
+	       {
+		line => 'Tue Aug 21 11:52:03 2007 192.168.45.184:60488 VERIFY ERROR: depth=0, error=unable to get local issuer certificate: /C=ES/ST=Nation/L=Nowhere/O=pajaros/CN=golondrina',
+		file => $macacoServer->logFile(),
+		expected => {
+			     event      => 'verificationIssuerError',
+			     timestamp => 'Tue Aug 21 11:52:03 2007',
+
+			     daemon_name => 'macaco',
+			     daemon_type => 'server',
+
+			     from_ip => '192.168.45.184',
+			     from_cert => '/C=ES/ST=Nation/L=Nowhere/O=pajaros/CN=golondrina',
+			    },
+	       },
+	       # verification error: incorrect common name
+	       {
+		line => 'Tue Aug 21 11:47:58 2007 192.168.45.184:52283 VERIFY X509NAME ERROR: /C=ES/ST=Nation/L=Nowhere/O=monos/CN=mandrill, must be gibbon',
+		file => $baboonServer->logFile(),
+		expected => {
+			     event      => 'verificationNameError',
+			     timestamp => 'Tue Aug 21 11:47:58 2007',
+
+			     daemon_name => 'baboon',
+			     daemon_type => 'server',
+
+			     from_ip => '192.168.45.184',
+			     from_cert => '/C=ES/ST=Nation/L=Nowhere/O=monos/CN=mandrill',
+			    },
+	       },
+	       # verification error: forged error to check non-defined error
+	       # behaviour
+	       {
+		line => 'Tue Aug 21 11:47:58 2007 192.168.45.184:52283 VERIFY UNKNOWN ERROR: the certificate was /C=ES/ST=Nation/L=Nowhere/O=monos/CN=mandrill, this a forged error to test default behaviour',
+		file => $baboonServer->logFile(),
+		expected => {
+			     event      => 'verificationError',
+			     timestamp => 'Tue Aug 21 11:47:58 2007',
+
+			     daemon_name => 'baboon',
+			     daemon_type => 'server',
+
+			     from_ip => '192.168.45.184',
+			     from_cert => '/C=ES/ST=Nation/L=Nowhere/O=monos/CN=mandrill',
+			    },
+	       },
+
+	       # client connection initialized
+	       {
+		line => 'Tue Aug 21 08:51:46 2007 192.168.45.184:54817 [mandrill] Peer Connection Initiated with 192.168.45.184:54817',
+		file => $macacoServer->logFile(),
+		expected => {
+			     event      => 'connectionInitiated',
+			     timestamp => 'Tue Aug 21 08:51:46 2007',
+
+			     daemon_name => 'macaco',
+			     daemon_type => 'server',
+
+			     from_ip => '192.168.45.184',
+			     from_cert => 'mandrill',
+			    },
+	       },
+
+	       #  connection to server initialized
+	       {
+		line => 'Mon Aug 27 06:51:47 2007 [server] Peer Connection Initiated with 192.168.45.126:10000',
+		file => $gibonClient->logFile(),
+		expected => {
+			     event      => 'serverConnectionInitiated',
+			     timestamp => 'Mon Aug 27 06:51:47 2007',
+
+			     daemon_name => 'gibon',
+			     daemon_type => 'client',
+
+			     from_ip => '192.168.45.126',
+			     from_cert => 'server',
+			    },
+	       },
+
+	       # client connection terminated
+	       {
+		line => 'Tue Aug 21 08:51:49 2007 mandrill/192.168.45.184:54817 Connection reset, restarting [0]',
+		file => $macacoServer->logFile(),
+		expected => {
+			     event      => 'connectionReset',
+			     timestamp => 'Tue Aug 21 08:51:49 2007',
+
+			     daemon_name => 'macaco',
+			     daemon_type => 'server',
+
+			     from_ip => '192.168.45.184',
+			     from_cert => 'mandrill',
+			    },
+	       },
+
+	       # server connection terminated
+	       {
+		line => 'Mon Aug 27 06:52:25 2007 Connection reset, restarting [0]',
+		file => $gibonClient->logFile(),
+		expected => {
+			     event      => 'connectionResetByServer',
+			     timestamp => 'Mon Aug 27 06:52:25 2007',
+
+			     daemon_name => 'gibon',
+			     daemon_type => 'client',
+			    },
+	       },
+
+
+	      );
+
+  foreach my $case (@cases) {
+    $dbEngine->clearLastInsert();
+
+    my $line = $case->{line};
+    my $file = $case->{file};
+
+
+    my $expected = $case->{expected};
+
+    # normalize expected missing fields
+    if (defined $expected) {
+      foreach my $field (qw(timestamp daemon_name daemon_type from_ip from_cert)) {
+	exists $expected->{$field} or
+	  $expected->{$field} = undef;
+      }
+
+    }
+
+    lives_ok {
+      $logHelper->processLine($file, $line, $dbEngine);
+    } 'processing line';
+
+    is_deeply $dbEngine->lastInsert, $expected, 'checking wether inserted data was the expected';
+  }
+
+}
+
+
+
+
+
+
+package FakeDBEngine;
+
+sub new
+{
+  my $class = shift;
+
+  my $self = { data => undef };
+  bless $self, $class;
+
+  return $self;
+
+}
+
+sub insert
+{
+  my ($self, $table, $data) = @_;
+  $self->{data} = $data;
+}
+
+sub lastInsert
+{
+  my ($self) = @_;
+  return $self->{data}
+}
+
+
+
+sub clearLastInsert
+{
+  my ($self) = @_;
+  $self->{data} = undef;
+}
+
+1;
---
+++ openvpn-1.4.2/src/EBox/OpenVPN/Model/Servers.pm
@@ -0,0 +1,416 @@
+# Copyright (C) 2007 Warp Networks S.L.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License, version 2, as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+# Class:
+#
+#
+
+#
+package EBox::OpenVPN::Model::Servers;
+use base qw(EBox::Model::DataTable EBox::OpenVPN::Model::InterfaceTable);
+
+use strict;
+use warnings;
+
+use EBox::Global;
+use EBox::Gettext;
+use EBox::Validate qw(:all);
+use EBox::Exceptions::External;
+use EBox::Exceptions::DataExists;
+
+use EBox::Types::HasMany;
+use EBox::Types::Text::WriteOnce;
+use EBox::Types::Boolean;
+use EBox::NetWrappers;
+
+use EBox::OpenVPN::Server;
+
+#use EBox::OpenVPN::Model::ServerConfiguration;
+use List::Util; # first
+
+use constant START_ADDRESS_PREFIX => '192.168.';
+use constant FROM_RANGE => 160;
+use constant TO_RANGE => 200;
+use constant PORTS => (1194, 11194 .. 11234);
+
+
+# Group: Public and protected methods
+
+sub new
+{
+    my $class = shift;
+    my %parms = @_;
+
+    my $self = $class->SUPER::new(@_);
+    bless($self, $class);
+
+    return $self;
+}
+
+sub _table
+{
+    my @tableHead =
+        (
+            new EBox::Types::Boolean (
+                                      fieldName => 'service',
+                                      printableName => __('Enabled'),
+                                      editable => 1,
+
+                                      defaultValue => 1,
+                                     ),
+            new EBox::Types::Text::WriteOnce
+                            (
+                                'fieldName' => 'name',
+                                'printableName' => __('Name'),
+                                'size' => '20',
+                                'unique' => 1,
+                                'editable' => 1,
+                             ),
+            new EBox::Types::HasMany
+                            (
+                                'fieldName' => 'configuration',
+                                'printableName' => __('Configuration'),
+                                'foreignModel' => 'ServerConfiguration',
+                                'view' => '/ebox/OpenVPN/View/ServerConfiguration',
+                                'backView' => '/ebox/OpenVPN/View/Servers',
+                                'size' => '1',
+                             ),
+            new EBox::Types::HasMany
+                            (
+                                'fieldName' => 'advertisedNetworks',
+                                'printableName' => __('Advertised networks'),
+                                'foreignModel' => 'ExposedNetworks',
+                                'view' => '/ebox/OpenVPN/View/ExposedNetworks',
+                                'backView' => '/ebox/OpenVPN/View/Servers',
+                                'size' => '1',
+                             ),
+            new EBox::Types::HasMany
+                            (
+                                'fieldName' => 'download',
+                                'printableName' => __('Download client bundle'),
+                                'foreignModel' => 'DownloadClientBundle',
+                                'view' => '/ebox/OpenVPN/View/DownloadClientBundle',
+                                'backView' => '/ebox/OpenVPN/View/Servers',
+                                'size' => '1',
+                             ),
+                             __PACKAGE__->interfaceFields(),
+          );
+
+    my $dataTable =
+        {
+            'tableName'              => __PACKAGE__->name(),
+            'printableTableName' => __('List of servers'),
+            'pageTitle' => __('VPN servers'),
+            'HTTPUrlView' => 'OpenVPN/View/Servers',
+            'automaticRemove' => 1,
+            'defaultController' => '/ebox/OpenVPN/Controller/Servers',
+            'defaultActions' => ['add', 'del', 'editField',  'changeView' ],
+            'tableDescription' => \@tableHead,
+            'class' => 'dataTable',
+            'printableRowName' => __('server'),
+            'sortedBy' => 'name',
+            'help' => _help(),
+            'modelDomain' => 'OpenVPN',
+        };
+
+    return $dataTable;
+}
+
+
+sub name
+{
+    __PACKAGE__->nameFromClass(),
+}
+
+# Method: precondition
+#
+#   Overrides  <EBox::Model::DataTable::precondition>
+#   to check if the CA is created otherwise this model can't be used
+#
+# Returns:
+#
+#       Boolean - true if the precondition is accomplished, false
+#       otherwise
+sub precondition
+{
+    my $global = EBox::Global->getInstance();
+    my $ca = $global->modInstance('ca');
+    return ($ca->isCreated());
+}
+
+# Method: preconditionFailMsg
+#
+#   Overrides <EBox::Model::DataTable::preconditionFailMsg
+#
+# Returns:
+#
+#       String - the i18ned message to inform user why this model
+#       cannot be handled
+#
+#
+sub preconditionFailMsg
+{
+    return  __x('{openpar}You need to create a CA certificate to use this module.'
+        . ' {closepar}{openpar}Please, go to the {openhref}certification '
+        . 'authority module{closehref} and create it.{closepar}',
+        openhref => qq{<a href='/ebox/CA/Index'>}, closehref => qq{</a>},
+        openpar => '<p>', closepar => '</p>' );
+
+}
+
+sub validateTypedRow
+{
+    my ($self, $action, $params_r, $actual_r) = @_;
+
+    if ($action eq 'add') {
+        $self->_checkCertificatesAvailable(
+                  __('Server creation')
+                                          );
+        return;
+    }
+
+    $self->_validateService($action, $params_r, $actual_r);
+    $self->_validateName($action, $params_r, $actual_r);
+}
+
+sub servers
+{
+    my ($self) = @_;
+    my @servers = map {
+        EBox::OpenVPN::Server->new(
+                                    $self->row($_)
+                                  )
+    } @{  $self->ids() };
+
+    return \@servers;
+
+}
+
+
+sub server
+{
+    my ($self, $name) = @_;
+    $name or
+        throw EBox::Exceptions::MissingArgument('name');
+
+    my $row = $self->findRow(name => $name);
+    defined $row or
+        throw EBox::Exceptions::Internal("Server $name does not exist");
+
+    return EBox::OpenVPN::Server->new($row);
+}
+
+
+sub serverExists
+{
+    my ($self, $name) = @_;
+    $name or
+        throw EBox::Exceptions::MissingArgument('name');
+
+    my $row = $self->findValue(name => $name);
+    return defined $row
+}
+
+sub addedRowNotify
+{
+    my ($self, $row) = @_;
+
+    EBox::OpenVPN::Model::InterfaceTable::addedRowNotify($self, $row);
+
+    $self->_configureVPN($row);
+    unless ($row->subModel('configuration')->configured()) {
+        my $service = $row->elementByName('service');
+        $service->setValue(0);
+        $row->store();
+    }
+    my $service = $row->elementByName('service');
+
+    if ($service->value()) {
+        my $openvpn = EBox::Global->modInstance('openvpn');
+        $openvpn->notifyLogChange();
+    }
+
+}
+
+sub updatedRowNotify
+{
+    my ($self, $row) = @_;
+
+    my $openvpn = EBox::Global->modInstance('openvpn');
+    $openvpn->notifyLogChange();
+
+}
+
+sub deletedRowNotify
+{
+    my ($self, $row) = @_;
+    my $name = $row->elementByName('name')->value();
+
+    my $openvpn = EBox::Global->modInstance('openvpn');
+    $openvpn->notifyDaemonDeletion($name, 'server');
+
+}
+
+# Group: Private methods
+
+sub _validateService
+{
+    my ($self, $action, $params_r, $actual_r) = @_;
+
+
+    if ( not exists $params_r->{service} ) {
+        return;
+    }
+
+    if (not $params_r->{service}->value()) {
+        return;
+    }
+
+    my $configuration = $actual_r->{'configuration'}->foreignModelInstance();
+    if ((not defined $configuration) or (not $configuration->configured())) {
+        throw EBox::Exceptions::External(
+                                         __('Cannot activate the server because '
+                                            .' is not fully configured; please '
+                                            . 'edit the configuration and retry')
+                                            )
+        }
+
+    unless ($self->precondition()) {
+        throw EBox::Exceptions::External(
+                __('Cannot create a server because there is not a CA certificate')
+                );
+    }
+}
+
+
+sub _validateName
+{
+    my ($self, $action, $params_r, $actual_r) = @_;
+
+    if ( not exists $params_r->{name} ) {
+        return;
+    }
+
+    my $name =  $params_r->{name}->value();
+    my $openvpn = EBox::Global->modInstance('openvpn');
+    $openvpn->checkNewDaemonName($name, 'server');
+}
+
+sub _checkCertificatesAvailable
+{
+    my ($self, $printableAction) = @_;
+
+    unless ($self->precondition()) {
+        throw EBox::Exceptions::External(
+                   __x(
+                       q/{act} not possible because there aren't/
+                       . ' any available certificate. Please, go to'
+                       . ' the certificate authority module'
+                       . ' and create new certificates',
+                       act => $printableAction
+                      )
+                                        );
+    }
+}
+
+# Return the model help message
+sub _help
+{
+    return __x('{openpar}You can configure openVPN servers to easily connect remote ' .
+            'offices or users.{closepar}' .
+            '{openpar}Click on {openit}Configuration{closeit} to set the VPN parameters.{closepar}' .
+            '{openpar}{openit}Advertised networks{closeit} allows you to configure which ' .
+            'networks you want to make accessible to the remote users.' .
+            '{openpar}Once you are done with the configuration you can download ' .
+            'a file bundle for your operating system to use in your clients.',
+            openpar => '<p>', closepar => '</p>', openit => '<i>',
+            closeit => '</i>');
+
+}
+
+# Configure VPN address, port and create a server certificate automatically
+sub _configureVPN
+{
+    my ($self, $row) = @_;
+
+    my $name = $row->valueByName('name');
+
+    # Configure network
+    my $networkMod = EBox::Global->modInstance('network');
+    my @addresses;
+    for my $iface (@{$networkMod->allIfaces()}) {
+        my $address = $networkMod->ifaceAddress($iface);
+        push (@addresses, $address) if ($address);
+    }
+
+    for my $id (@{$self->ids()}) {
+        next if ($id eq $row->id());
+        my $subModel = $self->row($id)->subModel('configuration');
+        my $vpn = $subModel->row()->elementByName('vpn')->printableValue();
+        my $name = $self->row($id)->valueByName('name');
+        push (@addresses, $vpn) if ($vpn);
+    }
+    my $network;
+    for my $postfix (FROM_RANGE .. TO_RANGE) {
+        my $net = START_ADDRESS_PREFIX . $postfix;
+        next if (List::Util::first {$_ =~ /^$net.*/ } @addresses);
+        $network= "${net}.0/24";
+        last;
+    }
+
+    # Configure  port
+    my $port;
+    my $firewall = EBox::Global->modInstance('firewall');
+    $port = List::Util::first { $firewall->availablePort('udp', $_) } PORTS;
+
+    # Create server certificate
+    my $ca = EBox::Global->modInstance('ca');
+    my $certName = "vpn-$name";
+    my @certs = @{$ca->listCertificates()};
+    unless (List::Util::first { $_->{dn}->{commonName} eq $certName } @certs ) {
+        my $caExpiration = $ca->getCACertificateMetadata()->{expiryDate};
+        $ca->issueCertificate(commonName => $certName , endDate => $caExpiration);
+    }
+
+    if ($port and $network) {
+        my $conf = $row->subModel('configuration');
+        my $subRow = $conf->row();
+        $subRow->elementByName('vpn')->setValue($network);
+        $subRow->elementByName('portAndProtocol')->setValue("$port/udp");
+        $subRow->elementByName('masquerade')->setValue(1);
+        $subRow->elementByName('certificate')->setValue($certName);
+        $subRow->store();
+    }
+
+    # Advertise local networks
+    for my $iface (@{$networkMod->InternalIfaces()}) {
+        next unless ($networkMod->ifaceMethod($iface) eq 'static');
+        for my $ifaceAddress (@{$networkMod->ifaceAddresses($iface)}) {
+            my $netAddress = EBox::NetWrappers::ip_network(
+                    $ifaceAddress->{address},
+                    $ifaceAddress->{netmask}
+                    );
+            my $advertise = $row->subModel('advertisedNetworks');
+            $advertise->add(
+                    network => EBox::NetWrappers::to_network_with_mask(
+                        $netAddress,
+                        $ifaceAddress->{netmask}
+                        )
+                    );
+        }
+    }
+}
+
+1;
